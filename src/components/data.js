11b62902ac1c262001b8cdaa659a7708530e51e1
Treveon <fasuke322@gmail.com>
2021-10-14 16:29:51 -0400

    added song covers

--- src/components/data.js
+++ src/components/data.js
@@ -76,7 +76,7 @@ function chillHop() {
 			cover:
 				"https://i.scdn.co/image/ab67616d0000b273672c6f8b33ed2e4fb65db30f",
 			artist: "Smile High, Teddy Roxpin",
-			audio: "https://mp3.chillhop.com/serve.php/?mp3=10478",
+			audio: "https://mp3.chillhop.com/?dl=1&track_id=23345&name=Ultraviolet",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
 			active: false,
@@ -101,7 +101,7 @@ function chillHop() {
 		},{
 			name: "khbbs",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "various",
 			audio: "https://downloads.khinsider.com/game-soundtracks/album/kingdom-hearts-birth-by-sleep-final-mix-gamerip/21.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -156,7 +156,7 @@ function chillHop() {
 		},{
 			name: "Forze dell'Oscurita",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "Yoko Shimomura",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-birth-by-sleep-358-2-days-original-soundtrack/qwbgurqc/3-27%20Forze%20dell%27Oscurita.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -165,7 +165,7 @@ function chillHop() {
 		},{
 			name: "Dismiss",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "Yoko Shimomura",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-birth-by-sleep-358-2-days-original-soundtrack/vjmhmhpj/2-21%20Dismiss.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -174,7 +174,7 @@ function chillHop() {
 		},{
 			name: "Birth by Sleep -A Link to the Future-",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "Yoko Shimomura",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-birth-by-sleep-358-2-days-original-soundtrack/qxerbyry/2-25%20Birth%20by%20Sleep%20-A%20Link%20to%20the%20Future-.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -183,7 +183,7 @@ function chillHop() {
 		},{
 			name: "Rage Awakened",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "Yoko Shimomura",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-ii-final-mix-gamerip/cdqkwcdijt/fm%20-%20Rage%20Awakened.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -192,7 +192,7 @@ function chillHop() {
 		},{
 			name: "Remembrance",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-ii-final-mix-gamerip/ohzcalibhj/fm%20-%20Cavern%20of%20Remembrance.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -201,7 +201,7 @@ function chillHop() {
 		},{
 			name: " L'Impeto Oscuro",
 			cover:
-				"",
+				"https://static.wikia.nocookie.net/kingdomhearts/images/3/38/Station_of_Awakening-_Sora_2_%28Art%29_KHII.png/revision/latest/scale-to-width-down/250?cb=20100413180123",
 			artist: "Yoko Shimomura",
 			audio: "https://vgmsite.com/soundtracks/kingdom-hearts-dream-drop-distance-original-soundtrack/qcqxfkwm/3-06.%20L%27Impeto%20Oscuro.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -210,7 +210,7 @@ function chillHop() {
 		},{
 			name: "Tal Tal Mountain Range",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/link_alt1.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/link-s-awakening-dx/fqnlalsxph/44%20-%20Tal%20Tal%20Mountain%20Range.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -220,7 +220,7 @@ function chillHop() {
 		{
 			name: " Hyrule Field Main Theme",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/link_alt1.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/legend-of-zelda-the-ocarina-of-time-2003-gc-gamerip/ghiqulfzxs/19%20-%20Hyrule%20Field%20Main%20Theme.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -229,7 +229,7 @@ function chillHop() {
 		},{
 			name: "King Dedede's Theme",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/kirby-the-very-best-of-kirby/2597-zjvjbuhwuj.jpg",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-08-kirby/rnrtmpnyfm/04.%20King%20Dedede%27s%20Theme%20%28Brawl%29.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -238,7 +238,7 @@ function chillHop() {
 		},{
 			name: "Fire Emblem Theme",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/qafofsydsz/01.%20Fire%20Emblem%20Theme.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -247,7 +247,7 @@ function chillHop() {
 		},{
 			name: "Shadow Dragon Medley",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/exofnqpttk/07.%20Shadow%20Dragon%20Medley.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -256,7 +256,7 @@ function chillHop() {
 		},{
 			name: "FE meet medley",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/yhteyiwtvb/17.%20Meeting%20Theme%20Series%20Medley.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -265,7 +265,7 @@ function chillHop() {
 		},{
 			name: "Fire Emblem - Mystery of the Emblem Medley",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/snoretybia/16.%20Fire%20Emblem%20-%20Mystery%20of%20the%20Emblem%20Medley.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -275,7 +275,7 @@ function chillHop() {
 		{
 			name: "The Edge of Dawn (Seasons of Warefare)",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/etemlfjvgy/54.%20The%20Edge%20of%20Dawn%20%28Seasons%20of%20Warefare%29.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -284,7 +284,7 @@ function chillHop() {
 		},{
 			name: "Three Houses Main Theme",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/tpavqnsksn/45.%20Fire%20Emblem%20-%20Three%20Houses%20Main%20Theme.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -293,7 +293,7 @@ function chillHop() {
 		},{
 			name: "Lost in Thoughts All Alone",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/fire_emblem.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-13-fire-emblem/wzoglurekb/39.%20Lost%20in%20Thoughts%20All%20Alone.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -302,7 +302,7 @@ function chillHop() {
 		},{
 			name: "Tal Tal Heights",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/link_alt1.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/pbkxlfwhge/15.%20Tal%20Tal%20Heights.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -311,7 +311,7 @@ function chillHop() {
 		},{
 			name: "Ocarina of Time Medley",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/link_alt1.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/wqfenallwv/16.%20Ocarina%20of%20Time%20Medley.mp3",
 			color: ["#205950", "#2ab3bf"],
@@ -320,7 +320,7 @@ function chillHop() {
 		},{
 			name: "The Legend of Zelda Medley",
 			cover:
-				"",
+				"https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/link_alt1.png",
 			artist: "",
 			audio: "https://vgmsite.com/soundtracks/super-smash-bros.-anthology-vol.-05-the-legend-of-zelda/iocjlkcngy/07.%20The%20Legend%20of%20Zelda%20Medley.mp3",
 			color: ["#205950", "#2ab3bf"],
